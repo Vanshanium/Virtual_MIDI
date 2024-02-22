0055f469ced47c9d2843c66a10f52539737faeaa
vansha <vansh.singh8272@gmail.com>
2024-02-22 22:41:46 +0530

    I am loving OOPS

--- Key_learning.cpp
+++ Key_learning.cpp
@@ -6,7 +6,6 @@ using namespace sf;
 using namespace std;
 
 
-int number_of_keys = 7;
 
 int piano_x = 560, piano_y = 400;
 
@@ -21,11 +20,9 @@ Color pressed_white(230,150,255);
 Color key_black(40,40,45);
 
 
-class key{
-    
+class Key_class{
 
-}
-
+private:
 
 RectangleShape make_key(Vector2f dimensions,Color key_color,int x_pos,int y_pos = 0)
 {
@@ -38,13 +35,154 @@ RectangleShape make_key(Vector2f dimensions,Color key_color,int x_pos,int y_pos 
 
 }
 
+Keyboard::Key Keymapping[12] = {Keyboard::Num1,
+Keyboard::Num2,
+Keyboard::Num3,
+Keyboard::Num4,
+Keyboard::Num5,
+Keyboard::Num6,
+Keyboard::Num7,
+Keyboard::Num8,
+Keyboard::Num9,
+Keyboard::Num0,
+Keyboard::Q,
+Keyboard::W,
+};
 
-void pressed_key_check()
-{
+class note{
+
+public:
+
+    RectangleShape shape;
+    string type;
+
+    note(string type,string sound,int position)
+    {
+        if (type == "white")
+        {
+            shape.setSize(white_key_dem);
+            shape.setPosition(position,0);
+
+        }
+        
+    }
+
+};
 
 
 
-}
+public:
+    
+    vector<RectangleShape> key_list;
+
+    vector<RectangleShape> white_keys;
+
+    vector<RectangleShape> black_keys;
+
+
+    // I Don't Know but this objects are not being defined outside of this functions!
+
+    Key_class()
+    {
+
+        //Defines all the white keys.
+        // Whites are defined first.
+        // Because they are in majority!!!!???
+
+        RectangleShape c4 = make_key(white_key_dem,key_white,2); 
+        key_list.push_back(c4);
+        white_keys.push_back(c4);
+
+        RectangleShape d4 = make_key(white_key_dem,key_white,82); 
+        key_list.push_back(d4);
+        white_keys.push_back(c4);
+
+        RectangleShape e4 = make_key(white_key_dem,key_white,162); 
+        key_list.push_back(e4);
+        white_keys.push_back(c4);
+
+        RectangleShape f4 = make_key(white_key_dem,key_white,242); 
+        key_list.push_back(f4);
+        white_keys.push_back(c4);
+
+        RectangleShape g4 = make_key(white_key_dem,key_white,322); 
+        key_list.push_back(g4);
+        white_keys.push_back(c4);
+
+        RectangleShape a4 = make_key(white_key_dem,key_white,402); 
+        key_list.push_back(a4);
+        white_keys.push_back(c4);
+
+        RectangleShape b4 = make_key(white_key_dem,key_white,482); 
+        key_list.push_back(b4);
+        white_keys.push_back(c4);
+
+        // Defining the Black Keys after whites
+        // Because they are in minority!
+
+        RectangleShape db = make_key(black_key_dem,key_black,55);
+        key_list.push_back(db);
+        black_keys.push_back(db);
+
+
+        RectangleShape eb = make_key(black_key_dem,key_black,135);
+        key_list.push_back(eb);
+        black_keys.push_back(eb);
+        
+        RectangleShape gb = make_key(black_key_dem,key_black,295);
+        key_list.push_back(gb);
+        black_keys.push_back(gb);
+
+        RectangleShape ab = make_key(black_key_dem,key_black,375);
+        key_list.push_back(ab);
+        black_keys.push_back(ab);
+
+        RectangleShape bb = make_key(black_key_dem,key_black,455);
+        key_list.push_back(bb);
+        black_keys.push_back(bb);
+    
+    }  
+
+
+
+    // Write what does this do!
+    void draw_keys(RenderWindow& window){
+
+        for(RectangleShape& note : key_list){
+
+            window.draw(note);
+        }
+    
+
+    }
+
+    void key_check(Event& event){
+
+        int i = 0;
+        for(Keyboard::Key& key_id : Keymapping){
+            if(event.key.code == key_id){
+
+                key_list[i].setFillColor(pressed_white);
+            }
+            i += 1;
+        }
+    }
+    
+    void key_recheck(Event& event){
+
+        int i = 0;
+        for(Keyboard::Key& key_id : Keymapping){
+            if(event.key.code == key_id){
+
+                key_list[i].setFillColor(key_white);
+            }
+            i += 1;
+        }         
+    }
+    
+
+};
+
 
 
 
@@ -53,67 +191,7 @@ int main()
 
     RenderWindow piano(VideoMode(piano_x,piano_y),"Piano_window");
 
-
-    vector<RectangleShape> all_keys(number_of_keys);
-
-
-    // Defining the White Keys.
-    RectangleShape c4 = make_key(white_key_dem,key_white,2); 
-    all_keys.push_back(c4);
-
-    RectangleShape d4 = make_key(white_key_dem,key_white,82); 
-    all_keys.push_back(d4);
-
-    RectangleShape e4 = make_key(white_key_dem,key_white,162); 
-    all_keys.push_back(e4);
-
-    RectangleShape f4 = make_key(white_key_dem,key_white,242); 
-    all_keys.push_back(f4);
-
-    RectangleShape g4 = make_key(white_key_dem,key_white,322); 
-    all_keys.push_back(g4);
-
-    RectangleShape a4 = make_key(white_key_dem,key_white,402); 
-    all_keys.push_back(a4);
-
-    RectangleShape b4 = make_key(white_key_dem,key_white,482); 
-    all_keys.push_back(b4);
-
-
-
-    // Defing the Black Keys
-
-
-
-    RectangleShape db = make_key(black_key_dem,key_black,55);
-    all_keys.push_back(b4);
-
-
-    RectangleShape eb = make_key(black_key_dem,key_black,135);
-
-    RectangleShape gb = make_key(black_key_dem,key_black,295);
-
-    RectangleShape ab = make_key(black_key_dem,key_black,375);
-
-    RectangleShape bb = make_key(black_key_dem,key_black,455);
-
-
-    Keyboard::Key Keymapping[number_of_keys];
-
-    Keymapping[0] = Keyboard::Num1;
-    Keymapping[1] = Keyboard::Num2;
-    Keymapping[2] = Keyboard::Num3;
-    Keymapping[3] = Keyboard::Num4;
-    Keymapping[4] = Keyboard::Num5;
-    Keymapping[5] = Keyboard::Num6;
-    Keymapping[6] = Keyboard::Num7;
-
-
-
-
-
-
-
+    Key_class mykeys;
 
     Event my_event;
 
@@ -135,32 +213,14 @@ int main()
 
             if(my_event.type == Event::KeyPressed){
                 
-                if(my_event.key.code == Keyboard::Q){
-                    
-                    c4.setFillColor(pressed_white);
-                    
+                mykeys.key_check(my_event);
+                
                 }
-                if(my_event.key.code == Keyboard::W){
-                    
-                    d4.setFillColor(pressed_white);
-                    
-                }
-            
-            }
 
             if(my_event.type == Event::KeyReleased){
             
-                if(my_event.key.code == Keyboard::Q){
-                        
-                        c4.setFillColor(key_white);
-                        
-                    }
-                    if(my_event.key.code == Keyboard::W){
-                        
-                        d4.setFillColor(key_white);
-                        
-                    }
-            
+                mykeys.key_recheck(my_event);
+
             }    
             
 
@@ -168,21 +228,7 @@ int main()
 
             piano.clear();
 
-            piano.draw(c4);
-            piano.draw(d4);
-            piano.draw(e4);
-            piano.draw(f4);
-            piano.draw(g4);
-            piano.draw(a4);
-            piano.draw(b4);
-
-            piano.draw(db);
-            piano.draw(eb);
-            piano.draw(gb);
-            piano.draw(ab);
-            piano.draw(bb);
-
-
+            mykeys.draw_keys(piano);
 
             piano.display();
 
