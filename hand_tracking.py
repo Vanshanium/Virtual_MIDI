bb995034919bcd99aab1d90128b23dffedab5a56
vansha <vansh.singh8272@gmail.com>
2024-02-25 23:00:15 +0530

    Getting the cordinates.

--- hand_tracking.py
+++ hand_tracking.py
@@ -3,21 +3,23 @@ import mediapipe as mp
 
 mphands = mp.solutions.hands
 hand_class = mphands.Hands()
-draw = mp.solutions.drawing_utils
 
 def get_hands(image):
+
+    finger_tip_indices = [4, 8, 12, 16, 20]
+    output_array = []
+    cords = []
 
     results = hand_class.process(image)
     
     i = 0
 
-    for marks in results.multi_hand_landmarks:
-        for landmarks in marks:
-            print("benkelode")
+    for landmarks in results.multi_hand_landmarks: 
 
+        for idx in finger_tip_indices:
+            cords.append(landmarks.landmark[idx].x)    
+            cords.append(landmarks.landmark[idx].y)
 
+            output_array.append(cords)
 
-
-    
-
-
+            cords = []
