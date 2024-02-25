49705f1fa7fc122a1bd5da3082739dd8e3ac4760
vansha <vansh.singh8272@gmail.com>
2024-02-25 14:48:34 +0530

    Everything set to work!

---
+++ opencv.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import hand_track
+
+
+# cap = cv.VideoCapture(0)
+
+
+frame = cv.imread("/home/vansha/Desktop/Code/Virtual_MIDI/assets/test_hand_image.jpg")
+
+
+
+# success,frame = cap.read()
+
+frame = cv.cvtColor(frame,cv.COLOR_BGR2RGB)
+
+hand_track.get_hands(frame)
+
+cv.imshow("MOYEEE",frame)
+
+cv.waitKey()
+
+
+# while(True):
+    
+#     success, frame = cap.read()
+
+#     frame = cv.cvtColor(frame,cv.COLOR_BGR2RGB)
+    
+#     hand_track.get_hands(frame)
+
+#     cv.imshow("FuCk",frame)
+    
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#             break
+
+
+
---
+++ preinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+sudo apt install -y libopencv-dev
+
+sudo apt install -y g++
+
+sudo apt install -y cmake 
+
+sudo apt install -y libsfml-dev
+
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
